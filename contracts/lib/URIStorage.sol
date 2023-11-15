942325f6eff80ab2b7552e4e3ebb7dd306423878
tenthirtyone <alexsherbuck@gmail.com>
2023-11-14 19:26:08 -0600

    natspec

--- contracts/lib/URIStorage.sol
+++ contracts/lib/URIStorage.sol
@@ -2,11 +2,26 @@
 
 pragma solidity ^0.8.9;
 
+/**
+ * @title URIStorage
+ * @dev Abstract contract for storing and managing URIs in ERC721-like tokens.
+ */
 abstract contract URIStorage {
+    // Mapping from token ID to their URIs
     mapping(uint256 => string) private _tokenURIs;
 
+    /**
+     * @dev Emitted when the metadata of a token is updated.
+     * @param tokenId The token ID whose metadata is updated.
+     */
     event MetadataUpdate(uint256 tokenId);
 
+    /**
+     * @notice Retrieves the URI associated with a given token.
+     * @dev Returns an empty string if no URI is set for the token.
+     * @param tokenId The ID of the token whose URI is being queried.
+     * @return string representing the URI of the given token ID.
+     */
     function tokenURI(
         uint256 tokenId
     ) public view virtual returns (string memory) {
@@ -14,9 +29,10 @@ abstract contract URIStorage {
     }
 
     /**
-     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
-     *
-     * Emits {MetadataUpdate}.
+     * @dev Internal function to set the URI for a given token.
+     * @dev Emits a {MetadataUpdate} event.
+     * @param _tokenId The token ID for which to set the URI.
+     * @param _tokenURI The URI to be set for the token.
      */
     function _setTokenURI(
         uint256 _tokenId,
@@ -26,6 +42,11 @@ abstract contract URIStorage {
         emit MetadataUpdate(_tokenId);
     }
 
+    /**
+     * @dev Internal function to retrieve the URI for a given token.
+     * @param _tokenId The token ID whose URI is being queried.
+     * @return string representing the URI of the given token ID, or an empty string if none is set.
+     */
     function _getTokenURI(
         uint256 _tokenId
     ) internal view virtual returns (string memory) {
