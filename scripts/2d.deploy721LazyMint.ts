e0ae7d922b5b02a6ce253d2ce08ef45f22ef34a4
tenthirtyone <alexsherbuck@gmail.com>
2024-05-31 12:57:17 -0500

    add 721 lazy mint deploy script

---
+++ scripts/2d.deploy721LazyMint.ts
@@ -0,0 +1,40 @@
+require("dotenv").config();
+import {
+  abi as FactoryABI,
+  bytecode as FactoryBytecode,
+} from "../artifacts/contracts/SingletonFactory.sol/SingletonFactory.json";
+import { bytecode as ERC721LazyMintBytecode } from "../artifacts/contracts/ERC721LazyMint.sol/ERC721LazyMint.json";
+
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+export const CONTRACT_SALT = "Dcentral.me";
+export function createSalt(string: string) {
+  return hre.ethers.utils.formatBytes32String(string);
+}
+const FACTORY_ADDRESS = "0x7eCcF14E9671eEA813d42cB42C5BE58f46980666";
+const SALT = createSalt(CONTRACT_SALT);
+
+async function main() {
+  const [owner, manager] = await hre.ethers.getSigners();
+  console.log(owner.address);
+
+  const factory = new hre.ethers.Contract(FACTORY_ADDRESS, FactoryABI, owner);
+
+  console.log(`Using factory at ${factory.address}`);
+
+  const tx = await factory.deploy(SALT, ERC721LazyMintBytecode, {
+    gasLimit: 8_000_000,
+  });
+
+  console.log(tx);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
